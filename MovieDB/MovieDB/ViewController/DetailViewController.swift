f0946c43c0026a736c311f33ed735ad24dd56250
Muhammad Farooq <raja.muhammad.farooq@gmail.com>
2019-04-23 00:55:36 +0500

    minor change

--- MovieDB/MovieDB/ViewController/DetailViewController.swift
+++ MovieDB/MovieDB/ViewController/DetailViewController.swift
@@ -12,6 +12,10 @@ class DetailViewController: UIViewController {
     
     var movie: Movie!
     
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieTextView: UITextView!
+    
     var movieDetail: MovieDetail! {
         didSet {
             updateView()
@@ -37,5 +41,24 @@ class DetailViewController: UIViewController {
     }
     
     func updateView() {
+        movieImageView.setImage(url: URL(string: movie.getLink()))
+        movieTitleLabel.text = movie.title
+        
+        let regularAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]
+        let boldAttributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24)]
+
+        let genresString = NSMutableAttributedString(string: "Genres ", attributes: boldAttributes)
+        let dateString = NSMutableAttributedString(string: "Date ", attributes: boldAttributes)
+        let overviewString = NSMutableAttributedString(string: "Overview ", attributes: boldAttributes)
+        
+        let sectionText = NSMutableAttributedString(string: " ")
+        sectionText.append(genresString)
+        sectionText.append(dateString)
+        sectionText.append(overviewString)
+        
+        movieTextView.attributedText = sectionText
+    }
+    
+    @IBAction func watchTrailer(_ sender: Any) {
     }
 }
